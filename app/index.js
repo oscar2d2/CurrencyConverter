a3c92e4ca26bd9d34116c31d9ec23db73d0989fe
oscar2d2 <oscarkong@gmail.com>
2018-04-15 12:04:28 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Changes you make will automatically reload.</Text>
+    <Text>It is from app/index.js</Text>
+  </View>
+);
+
+export default App;
