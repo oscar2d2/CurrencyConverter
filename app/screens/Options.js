0616ea53c1416e94bcecc627d026ef529866d682
oscar2d2 <oscarkong@gmail.com>
2018-05-03 23:42:13 +0800

    error handling

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -4,6 +4,7 @@ import { Ionicons } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 
 import { ListItem, Separator } from '../components/List';
+import { connectAlert } from '../components/Alert';
 
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
@@ -12,6 +13,7 @@ const ICON_SIZE = 23;
 class Options extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    alertWithType: PropTypes.func,
   }
   handleThemesPress = () => {
     console.log('press themes');
@@ -20,7 +22,7 @@ class Options extends Component {
 
   handleSitePress = () => {
     console.log('press site');
-    Linking.openURL('http://fixer.io').catch(() => alert('An error occur'));
+    Linking.openURL('http://fixer.io').catch(() => this.props.alertWithType('error', 'Sorry!', "Fixer.io can't be opened right now."));
   }
 
   render() {
@@ -48,4 +50,4 @@ class Options extends Component {
   }
 }
 
-export default Options;
+export default connectAlert(Options);
