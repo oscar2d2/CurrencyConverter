c7375d61b6e74902d280988557ba844e62b956c7
oscar2d2 <oscarkong@gmail.com>
2018-05-20 21:57:30 +0800

    Currency Reducer

---
+++ app/reducers/currencies.js
@@ -0,0 +1,28 @@
+import { SWAP_CURRENCY, CHANGE_CURRENCY_AMOUNT } from '../actions/currencies';
+
+const initialState = {
+  baseCurrency: 'USD',
+  quoteCurrency: 'GBP',
+  amount: 100,
+  conversions: {},
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHANGE_CURRENCY_AMOUNT:
+      return {
+        ...state,
+        amount: action.amount || 0,
+      };
+    case SWAP_CURRENCY:
+      return {
+        ...state,
+        baseCurrency: state.quoteCurrency,
+        quoteCurrency: state.baseCurrency,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
